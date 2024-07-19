a81fb9ff0b29415bc4588f46311b48b4a26d4990
Kilian Balaguer <hobbelig_nitraat.0b@icloud.com>
2024-07-19 12:38:52 +0200

    updated links

--- _config.js
+++ _config.js
@@ -1,6 +1,6 @@
 export const _config = {
-  repoUrl: "https://github.com/fun117/zshmgr",
-  docsRepositoryBase: "https://github.com/fun117/zshmgr/tree/main/website",
+  repoUrl: "https://github.com/WinUI-Blog/site/",
+  docsRepositoryBase: "https://github.com/WinUI-Blog/site/tree/main/",
 
   title: "WinUI Blog",
   origin: "https://winuiblog.vercel.app/",
